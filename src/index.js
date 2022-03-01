74d83e4de6fd11a93b724d77c60618a421d152ca
bmbaron <benjamin.m.baron@gmail.com>
2022-03-01 16:48:01 +0700

    rewrote data handling in global level functions

--- src/index.js
+++ src/index.js
@@ -41,9 +41,7 @@ function getCoordinates() {
   return result;
 }
 
-function getData(baseURL, coord, key) {
-  const url =
-    baseURL + coord.latitude + "&lon=" + coord.longitude + "&appid=" + key;
+function fetchJSON(url) {
   return fetch(url, { mode: "cors" })
     .then((res) => res.json())
     .then((data) => {
@@ -52,30 +50,45 @@ function getData(baseURL, coord, key) {
     .catch((e) => console.error(e.message, e.code));
 }
 
+function getWeatherFromJSON(JSON) {
+  const weatherData = userData;
+  weatherData.coordinates.latitude = JSON.coord.lat;
+  weatherData.coordinates.longitude = JSON.coord.lon;
+  weatherData.countryCode = JSON.sys.country;
+  let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+  weatherData.countryName = regionNames.of(JSON.sys.country);
+  weatherData.city = JSON.name;
+  weatherData.weather = JSON.weather[0].description;
+  weatherData.tempC = Math.round(JSON.main.temp - 273.15);
+  weatherData.tempF = Math.round((9 / 5) * weatherData.tempC + 32);
+  return weatherData;
+}
+
+function getPollutionFromJSON(JSON) {
+  const pollutionData = JSON.list[0].components.pm2_5;
+  return pollutionData;  
+}
+
 render(userData);
 
-getCoordinates().then((coordinates) => {
-  getData(apiQuery.baseURLs.weather, coordinates, apiQuery.API_KEY)
+getCoordinates().then((coord) => {
+  const weatherURL = apiQuery.baseURLs.weather + coord.latitude + "&lon=" + coord.longitude + "&appid=" + apiQuery.API_KEY;
+  const pollutionURL = apiQuery.baseURLs.pollution + coord.latitude + "&lon=" + coord.longitude + "&appid=" +  apiQuery.API_KEY;
+ 
+  const weather = fetchJSON(weatherURL)
     .then((data) => {
-      const newData = userData;
-      newData.coordinates.latitude = data.coord.lat;
-      newData.coordinates.longitude = data.coord.lon;
-      newData.countryCode = data.sys.country;
-      let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
-      newData.countryName = regionNames.of(data.sys.country);
-      newData.city = data.name;
-      newData.weather = data.weather[0].description;
-      newData.tempC = Math.round(data.main.temp - 273.15);
-      newData.tempF = Math.round((9 / 5) * newData.tempC + 32);
-      return newData;
-    })
-    .then((newData) => {
-      getData(apiQuery.baseURLs.pollution, coordinates, apiQuery.API_KEY).then(
-        (data) => {
-          newData.pm2_5 = data.list[0].components.pm2_5;
-          render(newData);
-        }
-      );
-    })
-    .catch((e) => console.error(e.message, e.code));
+      return getWeatherFromJSON(data);
+    });
+
+  const pollution = fetchJSON(pollutionURL)
+    .then((data) => {
+      return getPollutionFromJSON(data);
+    });
+
+  Promise.all([weather, pollution]).then((newData) => {
+    newData[0].pm2_5 = newData[1];
+    const finalData = newData[0];
+    render(finalData);
+  })
+  .catch((e) => console.error(e.message, e.code));
 });
